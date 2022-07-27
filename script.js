06f93b492e93e263c2ff92d5c63ae84667597e1f
szoker527 <szoker527@gmail.com>
2022-07-27 18:35:30 +0200

    Added new loops and made them create gird 16 by 16

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ const container = document.querySelector(".grid-container");
 
 function newGrid() {
     for (i = 16; i > 0; i--) {
-        console.log(i);
-        const div = document.createElement("div");
-        container.appendChild(div);
-        div.classList.add(".grid");
-    }
-}
 
+        const divBig = document.createElement("div");
+        container.appendChild(divBig);
+        divBig.classList.add("grid-big");
+
+        for (x = 16; x > 0; x--) {
+            const divSmall = document.createElement("div");
+            divBig.appendChild(divSmall);
+            divSmall.classList.add("grid-small");
+        }
+
+    }
+
+}
+//div.setAttribute('style', `height: ${size}px; width: ${size}px;`);   
 newGrid()
