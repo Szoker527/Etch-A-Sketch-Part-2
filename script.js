3dcef89cfa0994624368cb834bf702e47aafbf5a
szoker527 <szoker527@gmail.com>
2022-07-28 17:27:36 +0200

    Added modal

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
-
-
 const container = document.querySelector(".grid-container");
+const smallDivs =  document.querySelectorAll(".grid-small");
+const modal = document.getElementById("myModal");
+const gridBtn = document.getElementById("grid-button");
+const span = document.getElementsByClassName("close")[0];
+const gridSubmit = document.getElementById("grid-submit");
+const form = document.forms.gridForm
+let inputValue;
 
     for (i = 16; i > 0; i--) {
 
@@ -17,10 +21,35 @@ const container = document.querySelector(".grid-container");
 
     }
 
-const smallDivs =  document.querySelectorAll(".grid-small");
-
     smallDivs.forEach( function(div) {
         div.addEventListener("mouseover", (event) => {
             event.target.style.background = "orange";
         })
     })
+
+
+    gridBtn.onclick = function() {
+        modal.style.display = "flex";
+        modal.classList.add("modal-flex");
+      }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+      }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+
+
+
+    gridSubmit.addEventListener("click", function() {
+        inputValue = form.elements.input_value.value;
+        console.log(inputValue)
+        modal.style.display = "none";
+    })
+    
