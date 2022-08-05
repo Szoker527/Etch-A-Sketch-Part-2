245a995e4af6e76c0c336c65faf1dce500216f53
szoker527 <szoker527@gmail.com>
2022-08-05 19:26:29 +0200

    Fix color change works on mousedown

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const container = document.querySelector(".grid-container");
+let container = document.querySelector(".grid-container");
+const containerDivs = document.querySelectorAll(".grid-big");
 const modal = document.getElementById("myModal");
 const gridBtn = document.getElementById("grid-button");
 const gridReset = document.querySelector(".grid-reset");
@@ -6,57 +7,84 @@ const gridRandom = document.querySelector(".grid-random");
 const span = document.getElementsByClassName("close")[0];
 const gridSubmit = document.getElementById("grid-submit");
 const numberInput = document.getElementById("input-number");
-const form = document.forms.gridForm
-let containerChild = container.childNodes;
 const colorInput = document.getElementById("color");
+let containerChild = containerDivs.childNodes;
 let inputValue = 16;
+let color = "user";
+let colorChoice = "black";
+let mouseMove = false
+document.body.onmousedown = () => (mouseMove = true)
+document.body.onmouseup = () => (mouseMove = false)
+
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
 
     }
+}
 
+function changeColor(e) {
+    if (mouseMove === true) {
+        if (color === "random") {
+            e.target.style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
+            }
+        else if(color === "user") {
+            e.target.style.background = `${colorChoice}`;;
+        }
+        else if(color === "normal"){
+            console.log(e.target.classList.value);
+            e.target.style.background = `${colorChoice}`;
+        }
+    }
 }
 
 function gridDraw(gridAmount) {
-    containerChild = container.childNodes;
     for (i = 0; i < gridAmount; i++) {
         const divBig = document.createElement("div");
         container.appendChild(divBig);
         divBig.classList.add("grid-big");
-        
         for (x = 0; x < gridAmount; x++) {
             const divSmall = document.createElement("div");
             divBig.appendChild(divSmall);
             divSmall.classList.add("grid-small");
-            
+            divSmall.addEventListener("mousedown", changeColor)
+            divSmall.addEventListener("mouseover", changeColor)
+            containerChild = document.querySelectorAll(".grid-small")
         }
-        
     }
 }
 
-function randomColor() {
-    containerChild.forEach( function(div) {
-            div.addEventListener("mouseover", (event) => {
-                event.target.style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
-            })
-        })
-
-}
-
-function gridColor(nodelist, color) {
+function gridColor() { 
+    console.log("Wow!")
+    containerChild = document.querySelectorAll(".grid-small")
     if (color == "random") {
         randomColor()
+        }
+    else if(mouseMove === true) {
+        containerChild.forEach( div => div.addEventListener("mouseover", logText, {capture: false}))
+        }    
     }
-    else {
-        nodelist.forEach( function(div) {
-            div.addEventListener("mouseover", (event) => {
-                event.target.style.background = `${color}`;
-            })
-        })
-    }
-}
+
+
+    
+
+gridRandom.addEventListener("click", function() {
+    color = "random";
+})
+
+colorInput.addEventListener("change", function() {
+    color = "user"
+    colorChoice = `${this.value}`
+})
+
+gridReset.addEventListener("click", function() {
+    removeAllChildNodes(container);
+    gridDraw(inputValue);
+    colorInput.value = "#FFFF"
+    color = "normal"
+    colorChoice = "black";
+})
 
 gridBtn.onclick = function() {
     modal.style.display = "flex";
@@ -79,28 +107,17 @@ gridSubmit.addEventListener("click", function() {
             modal.style.display = "none";
             removeAllChildNodes(container);
             gridDraw(inputValue);
-            gridColor(containerChild, setBg = "black"); 
-            colorInput.value = "#000000"
+            colorInput.value = "#FFFF"
+            color = "normal"
+            colorChoice = "black";
         }
         else {
             console.log("ERROR")
         }
 })
 
-gridReset.addEventListener("click", function() {
-    removeAllChildNodes(container);
-    gridDraw(inputValue);
-    gridColor(containerChild, "black"); 
-    colorInput.value = "#000000"
-})
-
-
-gridRandom.addEventListener("click", randomColor)
-
-colorInput.addEventListener("change", function() {
-    gridColor(containerChild, this.value)
-})
-
-
 gridDraw(inputValue);
-gridColor(containerChild, "black");  
+window.onmousedown = () => {
+    // gridColor()
+}
+
